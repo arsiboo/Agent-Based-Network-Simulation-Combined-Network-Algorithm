bc80d0f360e172d75eedea265a2e1b907f4d9355
arsiboo <arsineh.boodaghian@gmail.com>
2022-10-07 18:01:23 +0200

    Update residuals.py

--- residuals.py
+++ residuals.py
@@ -1,66 +1,85 @@
 # Resodial flows
-import json
+# Residual_Capacity = Original_Capacity - current_flow
+# Residual_Capacity = Total_number_of_beds - current_people_in_queue
 import random
 import networkx as nx
 import xlrd
-import matplotlib.pyplot as plt
-from networkx.algorithms.flow import edmonds_karp, shortest_augmenting_path, preflow_push, dinitz, boykov_kolmogorov
+from networkx.algorithms.flow import edmonds_karp, shortest_augmenting_path, preflow_push, dinitz, boykov_kolmogorov, \
+    build_residual_network
 
 file = xlrd.open_workbook("mad_house.xlsx")
+data = xlrd.open_workbook("OutputExample.xlsx")
+
 hospital = file.sheet_by_name("Links")
 wards = file.sheet_by_name("Nodes")
-node_total_beds = []
-node_avg_serving_time = []
+output = data.sheet_by_name("queue_info")
+
 
 G = nx.DiGraph()
+RG = nx.DiGraph()
+
 for row in range(hospital.nrows):
     if row > 0:
         _data = hospital.row_slice(row)
-        if _data[2].value != 0:
-            _Node1 = _data[0].value
-            _Node2 = _data[1].value
-            G.add_edges_from(
-                [
-                    (_Node1, _Node2, {"patient_flow_rate": _data[2].value, "distribution_probability": _data[3].value})
-                ]
-            )
+        _Node1 = _data[0].value
+        _Node2 = _data[1].value
+        edge_label_input = _data[2].value
+        #G.add_edges_from([(_Node1, _Node2, int(edge_label_input))]) # knowing the label
+        G.add_edges_from(
+            [
+                (_Node1, _Node2, {"label_input": edge_label_input})
+            ]
+        )
 
-for row in range(wards.nrows):
+
+max_capacity = []
+patient_flow = []
+edge_label_output = []
+
+# Importing capacities
+for row in range(output.nrows):
     if row > 0:
-        _data = wards.row_slice(row)
-        node_total_beds.append(_data[1].value)
-        node_avg_serving_time.append(_data[2].value)
+        _data = output.row_slice(row)
+        patient_flow.append(_data[4].value)
+        edge_label_output.append(_data[6].value)
+        max_capacity.append(_data[9].value)
+
+print(len(max_capacity))
+print(max_capacity.pop())
+print(len(patient_flow))
+print(len(edge_label_output))
+
 
 node_list = list(G.nodes())
+#RG = build_residual_network(G, capacity="weight")
 
-# Assigning attribute to nodes:
-total_beds_dict = {k: v for k, v in zip(node_list, node_total_beds)}
-serving_time_dict = {k: v for k, v in zip(node_list, node_avg_serving_time)}
-nx.set_node_attributes(G, total_beds_dict, 'beds')
-nx.set_node_attributes(G, serving_time_dict, 'serving')
+# edge_weights = nx.get_edge_attributes(RG,"capacity")
+# RG.remove_edges_from((e for e, w in edge_weights.items() if w == 0))
 
-# calculating the edmonds karp flow:
-EK_R = edmonds_karp(G, "ward 1", "ward 6",capacity="patient_flow_rate")
-SAP_R = shortest_augmenting_path(G, "ward 1", "ward 6",capacity="patient_flow_rate")
-PP_R = preflow_push(G, "ward 1", "ward 6",capacity="patient_flow_rate")
-D_R = dinitz(G, "ward 1", "ward 6",capacity="patient_flow_rate")
-BK_R = boykov_kolmogorov(G, "ward 1", "ward 6",capacity="patient_flow_rate")
 
-print(G.edges.data())
-print(G)
-print(EK_R.edges.data())
-print(EK_R)
-print(SAP_R.edges.data())
-print(SAP_R)
-print(PP_R.edges.data())
-print(PP_R)
-print(D_R.edges.data())
-print(D_R)
-print(BK_R.edges.data())
-print(BK_R)
+#print("calculating different residual graphs for flow:")
 
-pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos, cmap=plt.get_cmap('jet'),
-                 node_color='pink', node_size=500, label=True)
+#EK_R = edmonds_karp(G, "ward_1", "ward_7", capacity="weight", residual=RG)
 
-# plt.show()
+# SAP_R = shortest_augmenting_path(G, "ward_1", "ward_7", capacity="weight", residual=RG)
+# PP_R = preflow_push(G, "ward_1", "ward_7", capacity="weight", residual=RG)
+# D_R = dinitz(G, "ward_1", "ward_7", capacity="weight", residual=RG)
+# BK_R = boykov_kolmogorov(G, "ward_1", "ward_7", capacity="weight", residual=RG)
+
+
+#for i in G.edges.data():
+#    print(i)
+#for i in EK_R.edges.data():
+#    print(i)
+
+# print(SAP_R.edges.data())
+# print(SAP_R)
+# print(PP_R.edges.data())
+# print(PP_R)
+# print(D_R.edges.data())
+# print(D_R)
+# print(BK_R.edges.data())
+# print(BK_R)
+
+# pos = nx.spring_layout(G)
+# nx.draw_networkx(G, pos, cmap=plt.get_cmap('jet'),node_color='pink', node_size=500, label=True)

5d6507e4b114ac5190d3bbd1c4f515b3552696a5
arsiboo <arsineh.boodaghian@gmail.com>
2022-10-12 19:39:40 +0200

    Update Percolation and Perturbation.py

--- Percolation and Perturbation.py
+++ Percolation and Perturbation.py
@@ -4,6 +4,10 @@ import networkx as nx
 import numpy as np
 import xlrd
 
+
+def perturbation():
+    return 0
+
 file = xlrd.open_workbook("mad_house.xlsx")
 data = xlrd.open_workbook("data.xlsx")
 
@@ -16,6 +20,7 @@ G_flow = nx.DiGraph()
 node_max_capacity = []
 edge_current_patients = []
 edge_transition = []
+upcoming_node = []
 node_overflow_state = []
 edge_label_output: List[Any] = []
 
@@ -24,12 +29,10 @@ for row in range(output.nrows):
     if row > 0:
         _data = output.row_slice(row)
         edge_label_output.append(_data[6].value)
+        upcoming_node.append(_data[8].value)
         edge_transition.append(_data[9].value)
         node_max_capacity.append(_data[10].value)
-        if _data[11].value < 0:
-            node_overflow_state.append(1)  # overflow state
-        else:
-            node_overflow_state.append(0.1)  # not overflow state
+        node_overflow_state.append(_data[11].value)
 
 nodes_mapping_list = []
 capacity_mapping_list = []
@@ -49,28 +52,16 @@ for row in range(hospital.nrows):
 
 nx.set_node_attributes(G_flow, 0.1, name='overflow_state')
 
-for label, state, capacity, transition in zip(edge_label_output, node_overflow_state, node_max_capacity,
-                                              edge_transition):
+for label, capacity, transition in zip(edge_label_output,  node_max_capacity, edge_transition):
     for u, v, net_edge in G_flow.edges(data=True):
         if net_edge['label_input'] == label:
             net_edge['per_capacity'] = capacity / transition
-            for k, net_node in G_flow.nodes(data=True):
-                if k == v:
-                    print(k)
-                    print(v)
-                    if state < 0:
-                        nx.set_node_attributes(net_node, {k: 1}, 'overflow_state')  # Overflowed
-                    elif state == 0:
-                        nx.set_node_attributes(net_node, {k: 0.5}, 'overflow_state')  # At Edge
-                    elif state > 0:
-                        nx.set_node_attributes(net_node, {k: 0.1}, 'overflow_state')  # No flowed
-                    print(G_flow.nodes.data())
 
-            #           print("Calculating percolation:")
 
-            #           print(nx.percolation_centrality(G_flow, attribute='overflow_state', weight='per_capacity'))
-        #        print("-----------------------------------------")
+for state, node in zip(node_overflow_state, upcoming_node):
+    for n, net_node in G_flow.nodes(data=True):
+        if n == node:
+            nx.set_node_attributes(G_flow, {n: state}, name='overflow_state')
+            print("Calculating percolation:")
+            print(nx.percolation_centrality(G_flow, attribute='overflow_state', weight='per_capacity'))
 
-# # for _node in G_flow.nodes.data():
-# # edge_weights = nx.get_edge_attributes(RG,"capacity")
-# # RG.remove_edges_from((e for e, w in edge_weights.items() if w == 0))
